72d20862068fa1b1d5c7108069f041e0f014f555
SindiCATT <80646595+SindiCATT@users.noreply.github.com>
2021-06-09 22:09:12 +0300

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import sys
+if __name__ == '__main__':
+    # Ввести список одной строкой.
+    a = list(map(float, input("Введите список --> ").split()))
+    c = float(input('Введите c: '))
+    # Переменная k для дальнейших вычислений после отрицательного элемента.
+    k = 0
+    # Если список пуст, завершить программу.
+    if not a:
+        print("Заданный список пуст", file=sys.stderr)
+        exit(1)
+    # Вывод меньших элементов
+    s = 0
+    for i in a:
+        if i < c:
+            s += 1
+    # Сумма после отрицательных элементов
+    n_0 = len(a)
+    for i, item in reversed(list(enumerate(a))):
+        if item < 0:
+            n_0 = i
+            break
+    for z in a:
+        k = sum(a[n_0 + 1:])
+    # Сортировка элементов (от большего к меньшему)
+    f = str(sorted(a, key=lambda x: abs(x - max(a))))
+    print(f"Количество элементов списка, меньших с --> {s}\nСумма после отрицательных элементов -- > {k}\n"
+          f"Упорядоченный список: {f}")
